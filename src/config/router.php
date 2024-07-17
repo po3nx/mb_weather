090d6c9b031694770bd5451928f99370e69449e0
Purwanto <ipo3nx@gmail.com>
2024-07-17 08:43:30 +0800

    Remove local routing

--- src/config/router.php
+++ src/config/router.php
@@ -5,16 +5,15 @@ use App\Controllers\WeatherController;
 $router = new Klein();
 $logger = require __DIR__ . '/logger.php';
 try {
-    $router->respond('GET', '/forecast_v2/', function ($request, $response, $service, $app) use ($logger) {
+    $router->respond('GET', '/', function ($request, $response, $service, $app) use ($logger) {
         $controller = new WeatherController($logger);
         return $controller->showWeather($request, $response, $service, $app);
     });
-    $router->respond('GET', '/forecast_v2/sync', function ($request, $response, $service, $app) use ($logger) {
+    $router->respond('GET', '/sync', function ($request, $response, $service, $app) use ($logger) {
         $controller = new WeatherController($logger);
         return $controller->syncWeather($request, $response, $service, $app);
     });
 } catch (Exception $error) {
-    echo "Test";
     $this->logger->error($error->getMessage(), ['exception' => $error]);
     $response->code(500);
     $response->send('Error Occured');
