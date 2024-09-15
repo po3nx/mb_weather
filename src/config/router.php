7802bc10aaf15c436347e72d87729153321c2e34
po3nx <ipo3nx@gmail.com>
2024-09-15 10:36:23 +0800

    Update router

--- src/config/router.php
+++ src/config/router.php
@@ -5,15 +5,20 @@ use App\Controllers\WeatherController;
 $router = new Klein();
 $logger = require __DIR__ . '/logger.php';
 try {
-    $router->respond('GET', '/', function ($request, $response, $service, $app) use ($logger) {
+    $router->respond('GET', '/forecast/', function ($request, $response, $service, $app) use ($logger) {
         $controller = new WeatherController($logger);
         return $controller->showWeather($request, $response, $service, $app);
     });
-    $router->respond('GET', '/sync', function ($request, $response, $service, $app) use ($logger) {
+    $router->respond('GET', '/forecast/check', function ($request, $response, $service, $app) use ($logger) {
+        $controller = new WeatherController($logger);
+        return $controller->checkWeather($request, $response, $service, $app);
+    });
+    $router->respond('GET', '/forecast/sync', function ($request, $response, $service, $app) use ($logger) {
         $controller = new WeatherController($logger);
         return $controller->syncWeather($request, $response, $service, $app);
     });
 } catch (Exception $error) {
+    echo "Test";
     $this->logger->error($error->getMessage(), ['exception' => $error]);
     $response->code(500);
     $response->send('Error Occured');
